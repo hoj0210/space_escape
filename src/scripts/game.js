57bdeb0c575df7c2c82364b8d8cc674168a6e15c
Hojung Cha <hoj0210@gmail.com>
2021-02-18 20:30:20 -0800

    commiting changes

--- src/scripts/game.js
+++ src/scripts/game.js
@@ -14,6 +14,7 @@ class Game {
         this.showScore = this.showScore.bind(this);
         this.updateScore = this.updateScore.bind(this);
         this.songChoice = 0;
+        this.synth1 = new Tone.Synth().toDestination();
         this.bricks = [];
         this.intervalId = 0;
         this.every = false;
